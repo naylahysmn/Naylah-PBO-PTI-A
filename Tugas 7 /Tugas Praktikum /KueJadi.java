54ef6fd8ad2e0aa13413dac667baf3b0cc7c2040
naylahysmn <160575552+naylahysmn@users.noreply.github.com>
2024-05-08 20:56:44 +0700

    bang udah bang

---
+++ Tugas 7 /Tugas Praktikum /KueJadi.java
@@ -0,0 +1,17 @@
+class KueJadi extends Kue {
+    private double jumlah;
+    
+    public KueJadi(String nama, double harga, double jumlah) {
+        super(nama, harga);
+        this.jumlah = jumlah;
+    }
+    
+    @Override
+    public double hitungHarga() {
+        return getHarga() * jumlah * 2;
+    }
+
+    public double getJumlah() {
+        return jumlah;
+    }
+}
