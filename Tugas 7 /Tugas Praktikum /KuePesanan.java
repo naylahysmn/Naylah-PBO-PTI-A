8c9d1334d7c555bb26afc4b5eedc803af9ecb4aa
naylahysmn <160575552+naylahysmn@users.noreply.github.com>
2024-05-08 20:57:12 +0700

    bang udah bang

---
+++ Tugas 7 /Tugas Praktikum /KuePesanan.java
@@ -0,0 +1,17 @@
+class KuePesanan extends Kue {
+    private double berat;
+    
+    public KuePesanan(String nama, double harga, double berat) {
+        super(nama, harga);
+        this.berat = berat;
+    }
+    
+    @Override
+    public double hitungHarga() {
+        return getHarga() * berat;
+    }
+    
+    public double getBerat() {
+        return berat;
+    }
+}
