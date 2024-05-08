83f087f34b93b0b82260e24fc09f16e612c5b022
naylahysmn <160575552+naylahysmn@users.noreply.github.com>
2024-05-08 20:55:51 +0700

    bang udah bang

---
+++ Tugas 7 /Tugas Praktikum /Kue.java
@@ -0,0 +1,24 @@
+abstract class Kue {
+    private String nama;
+    private double harga;
+    
+    public Kue(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+    
+    public abstract double hitungHarga();
+    
+    @Override
+    public String toString() {
+        return "Nama Kue: " + nama + ", Harga: " + hitungHarga();
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+}
