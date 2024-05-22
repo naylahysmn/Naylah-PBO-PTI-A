501dc1377c809c5b5a5b7275c518a122f7ebbd4a
naylahysmn <160575552+naylahysmn@users.noreply.github.com>
2024-05-22 13:49:44 +0700

    done ya kak

---
+++ Tugas 9 /Analisis Percobaan /Identitas.java
@@ -0,0 +1,6 @@
+public interface Identitas {
+    // No.4
+    // Terjadi eror karena kelas manusia mengoveride tampilkan nama identitas
+    public void tampilkanNama();
+    public void tampilkanUmur();
+}   
